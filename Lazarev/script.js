a780526bbcbf02be0d2e193aeaf1fb24fba24c8b
PranjeetGoswami <prgoswami2020@gmail.com>
2024-11-16 21:42:28 +0530

    add to repo

--- Lazarev/script.js
+++ Lazarev/script.js
@@ -40,3 +40,52 @@ nav.addEventListener("mouseleave", function () {
 
 }
 navAnimation()
+
+
+function page2Animation(){
+    var rightElem = document.querySelectorAll('.right-elem')
+    rightElem.forEach(function(elem){
+    elem.addEventListener('mouseenter',function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:1,
+            scale:1
+        })
+    })
+    elem.addEventListener('mouseleave',function(){
+        gsap.to(elem.childNodes[3],{
+            opacity:0,
+            scale:0
+        })
+    })
+    elem.addEventListener('mousemove',function(dets){
+        gsap.to(elem.childNodes[3],{
+            x:dets.x - elem.getBoundingClientRect().x-90,
+            y:dets.y - elem.getBoundingClientRect().y-115
+        })
+    })
+})
+}
+page2Animation()
+
+function page3VideoAnimation(){
+    var page3Center = document.querySelector('.page3-center')
+var video = document.querySelector('#page3 video')
+
+page3Center.addEventListener('click',function(){
+    video.play()
+    gsap.to(video,{
+        transform:'scaleX(1) scaleY(1)',
+        opacity:1,
+        borderRadius: 0
+    })
+})
+video.addEventListener('click',function(){
+    video.pause()
+    gsap.to(video,{
+        transform:'scaleX(0.7) scaleY(0)',
+        opacity:0,
+        borderRadius:"30px"
+    })
+})
+}
+page3VideoAnimation()
